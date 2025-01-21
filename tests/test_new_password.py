82e1baa62d22e8e862e6f33d3f9dba0cd82ec5d3
rafael <rafshtein1912@gmail.com>
2025-01-21 14:29:07 +0300

    comment

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -15,3 +15,14 @@ def test_password_characters():
 Тест, что длина пароля соответствует заданной
 Тест, что два сгенерированных подряд пароля различаются
 """
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    for length in range(1, 21): 
+        assert len(generate_password(length)) == length
+
+def test_password_uniqueness():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2
+
